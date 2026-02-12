e291c417837d6b5e91f6e451aee30a0e1873fa6a
LAPTOP-ECQK55FH\Rmm_2 <casiroga89@gmail.com>
2026-02-11 20:48:00 -0600

    Added envelope animation and vibration

--- app.py
+++ app.py
@@ -3,14 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def password():
+    return render_template("password.html")
+
+@app.route("/propuesta")
 def home():
     name = "Nat"
     return render_template("index.html", name=name)
-
-@app.route("/yes")
-def yes():
-    name = "Nat"
-    return render_template("yes.html", name=name)
-
-if __name__ == "__main__":
-    app.run()
